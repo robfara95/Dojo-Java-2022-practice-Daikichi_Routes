1919e87d06c30fe72d0c0e48e887d00988f3b372
robfara95 <robfara@gmail.com>
2022-01-26 15:27:39 -0500

    Practice - Daikichi Routes version 1.0

---
+++ src/test/java/com/codingdojo/daikichi_routes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichi_routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
